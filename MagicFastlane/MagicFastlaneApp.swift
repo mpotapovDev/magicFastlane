d75ba07cbacdbe6cad26d023e4dc51e35874456c
mpotapov <mpotapovbusiness@gmail.com>
2023-12-29 16:36:40 +0200

    first commit

---
+++ MagicFastlane/MagicFastlaneApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagicFastlaneApp.swift
+//  MagicFastlane
+//
+//  Created by Maxim Potapov on 29.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct MagicFastlaneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
